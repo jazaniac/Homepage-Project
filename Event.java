158d8a1cdeac9d5b5418abb6c739978535691404
travrase42 <travrase42@gmail.com>
2015-09-24 08:59:25 -0600

    when shereyas does nothing...

---
+++ Event.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Event {
+	
+	private int startTime;
+	private int endTime;
+	private String eventName;
+	private GregorianCalendar cal = new GregorianCalendar();  
+	
+	public Event(int startTime, int endTime, String eventName, int year, int month, int day) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.eventName = eventName;
+		cal.set(year, month, day);
+		}
+	
+	public Event(int startTime, int endTime, String eventName) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.eventName = eventName;
+		}
+
+	public GregorianCalendar getDate(){
+		return cal;
+	}
+	// Travis Made This
+	public int getStartTime(){ 
+		return startTime;
+	}
+	
+	public int getEndTime(){ 
+		return endTime;
+	}
+	
+	public int getDay(){ 
+		return cal.get(GregorianCalendar.DAY_OF_MONTH);
+	}
+}
